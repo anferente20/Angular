713514e1bf340a7761f93985c5a1fce6da7cc510
Andrés Rentería <afrenteriav@correo.udistrital.edu.co>
2020-05-28 16:07:09 -0500

    Corrección funcionamiento de los botones

--- CursosAcmeVision/src/app/components/about/about.component.ts
+++ CursosAcmeVision/src/app/components/about/about.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   cursos = [
-    {nombre : 'Angular', img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'},
-    ];
+    {nombre : 'Angular', estado: false, img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'},
+    {nombre : 'Java', estado: false, img : 'https://i.blogs.es/e7b69c/java_logo/1366_2000.png'},
+    {nombre : 'Python', estado: false, img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png'}  
+  ];
   inscrito = false;
   constructor() { }
   inscribirse(): void{
