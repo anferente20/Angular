2c596f8284c947f0d3c97a39f4032b8cb29ec676
Andrés Rentería <afrenteriav@correo.udistrital.edu.co>
2020-05-28 20:11:44 -0500

    Corrección final Botones

--- CursosAcmeVision/src/app/home/home.component.ts
+++ CursosAcmeVision/src/app/home/home.component.ts
@@ -9,8 +9,8 @@ export class HomeComponent implements OnInit {
   cursos = [
     {nombre : 'Angular', img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'},
     {nombre : 'Java', img : 'https://i.blogs.es/e7b69c/java_logo/1366_2000.png'},
-    {nombre : 'Python', img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png'}
-  ];
+    {nombre : 'Python', img : 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1200px-Python-logo-notext.svg.png'},
+   ];
   constructor() { }
 
   ngOnInit(): void {
